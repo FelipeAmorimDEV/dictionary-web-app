0740774648f26e15353c882a6e56e02a48c7680b
FelipeAmorimDEV <felipecastroo.ads@gmail.com>
2024-04-05 12:52:26 -0300

    ✔  Global css abstraido para arquivo próprio

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './app.jsx'
+import './global.css'
 import './styles.css'
 
 const rootElement = document.querySelector("[data-js='root']")
